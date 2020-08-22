2864f396e3241bfe192f148c488647338fb4140f
Hanleyjames <hanley.doth@outlook.com>
2020-08-22 11:21:41 -0700

    add person radius and coordinates

---
+++ person.py
@@ -0,0 +1,9 @@
+#define the radius of the person
+person_head_radius = 10
+#define person start points
+person_one_x_start = 20
+person_one_y_start = 20
+person_two_x_start = 30
+person_two_y_start = 20
+person_three_x_start = 40
+person_three_y_start = 20
