3d01e543f8e55c109b8b7f668bc45045b30ff9f9
Hanleyjames <hanley.doth@outlook.com>
2020-08-22 14:39:29 -0700

    Update person file to have crowds

--- person.py
+++ person.py
@@ -7,3 +7,20 @@ person_two_x_start = 30
 person_two_y_start = 20
 person_three_x_start = 40
 person_three_y_start = 20
+#Not really sure yet why window needs to be created
+def create_group(window, canvas, xinc, yinc):
+    crowd = canvas.create_oval(person_one_x_start - person_head_radius,
+                person_one_y_start - person_head_radius,
+                person_one_x_start + person_head_radius,
+                person_one_y_start + person_head_radius,
+                fill="blue", outline="black", width=1),
+            canvas.create_oval(person_two_x_start - person_head_radius,
+                person_two_y_start - person_head_radius,
+                person_two_x_start + person_head_radius,
+                person_two_y_start + person_head_radius,
+                fill="blue", outline="black", width=1),
+            canvas.create_oval(person_three_x_start - person_head_radius,
+                person_three_y_start - person_head_radius,
+                person_three_x_start + person_head_radius,
+                person_three_y_start + person_head_radius,
+                fill="blue", outline="black", width=1)
