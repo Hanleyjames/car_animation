c3369ada3c8785d24c106156a3eb93482a75c421
Hanleyjames <hanley.doth@outlook.com>
2020-08-24 19:40:37 -0700

    wah

--- person.py
+++ person.py
@@ -1,15 +1,9 @@
 #define the radius of the person
 person_head_radius = 10
-#define person start points
-#person_one_x_start = 20
-person_one_y_start = 20
-person_two_x_start = 30
-person_two_y_start = 20
-person_three_x_start = 40
-person_three_y_start = 20
-#Not really sure yet why window needs to be created
+#create groups of people
 def create_group(canvas):
     person_one_x_start = 20
+    person_one_y_start = 20
     for i in range(0,5):
         crowd = canvas.create_oval(person_one_x_start - person_head_radius,
                 person_one_y_start - person_head_radius,
